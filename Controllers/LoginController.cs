dbd14f48279b56403f0ba5fa94c00601f91ce00d
Vietanh2332 <anhndv.114010121012@vtc.edu.vn>
2023-07-21 17:14:30 +0700

    commit

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProject.Controllers{
+  public class LoginController : Controller
+  {
+
+      public string Index()
+      {
+          return "Hello Word";
+      }
+  }
+}
