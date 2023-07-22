293b70588a3f6a354a7ca92f894b7ad5c203252e
Vietanh2332 <anhndv.114010121012@vtc.edu.vn>
2023-07-22 23:51:42 +0700

    commit

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using MyProject.Models; // Import namespace chứa LoginModel
+
+namespace MyProject.Controllers
+{
+    public class RegisterController : Controller
+    {
+        // Phương thức GET để hiển thị form đăng nhập
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // Phương thức POST để xử lý yêu cầu khi nhấn nút Sign In
+        [HttpPost]
+        public IActionResult SignUp(string email, string password, bool rememberMe)
+        {
+            // Gọi phương thức CheckCredentials từ LoginModel để kiểm tra thông tin đăng nhập
+            bool isValidCredentials = LoginModel.CheckCredentials(email, password);
+
+            if (isValidCredentials)
+            {
+                // Xử lý khi thông tin đăng nhập hợp lệ
+                // Chẳng hạn, chuyển hướng đến trang chính
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                // Xử lý khi thông tin đăng nhập không hợp lệ
+                // Chẳng hạn, hiển thị thông báo lỗi
+                ViewBag.ErrorMessage = "Invalid email or password.";
+                return View("Index");
+            }
+        }
+    }
+}
